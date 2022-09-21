ef0d2d6a8cf38f90029bd96eb24ba6702a7de4bb
Bankts <106667102+Bankts@users.noreply.github.com>
2022-09-21 19:49:22 +0100

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -49,7 +49,7 @@ const Contact = () => {
             </div>
             <div className="c-info-item">
               <img className="c-icon" src={Address} alt="" />
-              60, Fagbeyiro Abayomi Street, Alakuko, Lagos.
+              Lagos, Nigeria.
             </div>
           </div>
         </div>
